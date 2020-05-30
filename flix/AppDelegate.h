bf4a2f55cae4a6a5ae43e348d70531968237bfc7
Alexei Tulloch <ajt254@cornell.edu>
2020-05-30 13:15:38 -0400

    Initial Commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by Alexei Tulloch on 5/30/20.
+//  Copyright © 2020 Alexei Tulloch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flix
+//
+//  Created by Alexei Tulloch on 5/30/20.
+//  Copyright © 2020 Alexei Tulloch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
